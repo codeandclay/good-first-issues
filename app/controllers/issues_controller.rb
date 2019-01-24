5a82afacaafa58be8eb340b6434a68d78b315b1c
O Denman <oliver@codeandclay.com>
2019-01-24 15:03:02 +0000

    Make index page title relevant to issues listed
    If the issues are filtered by language or label, the title should reflect this.

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -7,9 +7,26 @@ class IssuesController < ApplicationController
     else
       @issues = paginated(Issue)
     end
+    @title = title
   end
 
   private
+
+  def title
+    # language_title || issue_title || base_title
+  end
+
+  def language_title
+    "#{params[:language]} issues" if params[:language]
+  end
+
+  def issue_title
+    "Issues labelled #{params[:label]}" if params[:label]
+  end
+
+  def base_title
+    'All issues'
+  end
 
   def paginated(collection)
     collection.paginate(page: params[:page])
