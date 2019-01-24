bad04093d610aaae97ec4fc0a4bc1d5d151fc833
O Denman <oliver@codeandclay.com>
2019-01-24 13:11:53 +0000

    Filter by label when label is clicked
    A user can click on a label and only issues tagged with that label will be
    shown.

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,5 +1,15 @@
 class IssuesController < ApplicationController
   def index
-    @issues = Issue.paginate(page: params[:page])
+    if tag = safe_params[:tag]
+      @issues = Label.find_by(name: tag).issues.paginate(page: params[:page])
+    else
+      @issues = Issue.paginate(page: params[:page])
+    end
+  end
+
+  private
+
+  def safe_params
+    params.permit(:tag)
   end
 end
