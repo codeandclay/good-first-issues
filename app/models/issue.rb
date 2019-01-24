7eb5f02035bf96732359616712bc806002e8b917
O Denman <oliver@codeandclay.com>
2019-01-24 20:47:24 +0000

    Use scopes for by_labels and by_language instead of methods

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -2,11 +2,11 @@ class Issue < ApplicationRecord
   has_and_belongs_to_many :labels
   default_scope { order(created_at: :desc) }
 
-  def self.by_labels(labels)
+  scope :by_labels, lambda { |labels|
     joins(:labels).where(labels: { name: labels })
-  end
+  }
 
-  def self.by_language(language)
+  scope :by_language, lambda { |language|
     where(language: language)
-  end
+  }
 end
