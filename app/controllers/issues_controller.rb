0f66865051acd3dbb710df5a81e98c95931b1fa9
O Denman <oliver@codeandclay.com>
2019-01-24 18:03:07 +0000

    Extract index page title to class
    This is in anticipation of filtering issues by multiple labels and is a step
    towards that.

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -7,26 +7,10 @@ class IssuesController < ApplicationController
     else
       @issues = paginated(Issue.all.includes(:labels))
     end
-    @title = title
+    @title = IndexTitle.new(params).to_s
   end
 
   private
-
-  def title
-    language_title || issue_title || base_title
-  end
-
-  def language_title
-    "#{params[:language]} issues" if params[:language]
-  end
-
-  def issue_title
-    "Issues labelled #{params[:label]}" if params[:label]
-  end
-
-  def base_title
-    'All issues'
-  end
 
   def paginated(collection)
     collection.paginate(page: params[:page])
---
+++ app/lib/index_title.rb
@@ -0,0 +1,29 @@
+class IndexTitle
+  def initialize(params)
+    @params = params
+  end
+
+  def to_s
+    language_title || issue_title || base_title
+  end
+
+  private
+
+  attr_accessor :params
+
+  def language_title
+    "#{params[:language]} issues" if params[:language]
+  end
+
+  def issue_title
+    "Issues labelled #{params[:label]}" if params[:label]
+  end
+
+  def base_title
+    'All issues'
+  end
+
+  def paginated(collection)
+    collection.paginate(page: params[:page])
+  end
+end
