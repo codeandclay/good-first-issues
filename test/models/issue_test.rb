a685ac393472362d6a0dd9e71b85dcc129a86785
O Denman <oliver@codeandclay.com>
2019-01-24 13:56:09 +0000

    Add regression tests for Issue::by_language() and ::by_label()

--- test/models/issue_test.rb
+++ test/models/issue_test.rb
@@ -11,4 +11,15 @@ class IssueTest < ActiveSupport::TestCase
     row.labels << Label.create(name: 'Label Two')
     assert_equal 2, row.labels.count
   end
+
+  test 'A collection of issues is returned when a label name is given' do
+    label = Label.create(name: 'Label')
+    10.times { label.issues << Issue.create }
+    assert_equal 10, Issue.by_label('Label').count
+  end
+
+  test 'A collection of issues is returned whena language name is given' do
+    10.times { Issue.create(language: 'Test') }
+    assert_equal 10, Issue.by_language('Test').count
+  end
 end
