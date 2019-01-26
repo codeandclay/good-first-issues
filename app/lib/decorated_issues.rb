f65f2bd1f4c85472673830555c19a8d67a36cee9
O Denman <oliver@codeandclay.com>
2019-01-26 21:33:35 +0000

    Adapt title to index results

--- app/lib/decorated_issues.rb
+++ app/lib/decorated_issues.rb
@@ -2,6 +2,19 @@ class DecoratedIssues < SimpleDelegator
   include ActionView::Helpers::TextHelper
 
   def title
+    return default_title if where_clause.to_h.empty?
+
+    specific_title
+  end
+
+  private
+
+  def default_title
     "Found #{pluralize(total_entries, 'issues')}"
+  end
+
+  def specific_title
+    "Found #{pluralize(total_entries, 'issues')} "\
+     "matching #{where_clause.to_h.values.join(' + ')}"
   end
 end
