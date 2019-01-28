921442d01c23fae01541ff81f7b644cc425effac
O Denman <oliver@codeandclay.com>
2019-01-28 17:48:03 +0000

    Set root to issues#index

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :issues, only: :index
   resources :tags, only: :index
+
+  root to: 'issues#index'
 end
