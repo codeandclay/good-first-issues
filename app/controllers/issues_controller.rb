633a01ebbc5cf2927e7c2e1463178272cdc81e1e
O Denman <oliver@codeandclay.com>
2019-01-30 16:39:34 +0000

    Remove unwanted comments

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -6,15 +6,6 @@ class IssuesController < ApplicationController
   private
 
   def paginated_issues
-    # When referencing the tables by symbol, the returned records only
-    # include the matching labels:
-    # > issues.includes(:labels, :language).first.labels.map(&:name)
-    # => ["bug"]
-    #
-    # The expected result is:
-    # => ["bug", "help wanted"]
-    # I don't yet understand why but it works as expected when referencing the
-    # tables by string.
     issues.paginate(page: params[:page])
   end
 
@@ -28,15 +19,6 @@ class IssuesController < ApplicationController
 
   def issues
     return Issue.by_language_and_labels(language, labels) if language && labels
-
-    # TODO: I would prefer to do a union here to but the models are not
-    # structurally compatible.
-    #
-    # return Issue.all unless labels || language
-    # Issue.by_labels(labels).or Issue.by_language(language)
-    #
-    # ArgumentError (Relation passed to #or must be structurally compatible.
-    # Incompatible values: [:joins])
     return Issue.by_labels(labels) if labels
     return Issue.by_language(language) if language
 
