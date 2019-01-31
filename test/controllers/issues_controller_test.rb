ae63feb0c495764292b077e20fb365e808344df6
O Denman <oliver@codeandclay.com>
2019-01-31 13:25:17 +0000

    Test that row has multiple labels

--- test/controllers/issues_controller_test.rb
+++ test/controllers/issues_controller_test.rb
@@ -29,24 +29,20 @@ class IssuesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  # Note: I cannot get this to fail so I have commented it out. See note in
-  # the contoller. Once I know what's going on, I expect I will be able to
-  # implement this test.
+  test 'The controller returns issues with expected labels per row' do
+    language = Language.new(name: 'C')
+    label_a = Label.new(name: 'label a')
+    label_b = Label.new(name: 'label b')
+    Issue.create(
+      language: language,
+      labels: [label_a, label_b],
+      url: 'https://example.com',
+      user_avatar_url: 'https://example.com/image.png'
+    )
 
-  # test 'The controller returns issues with expected labels per row' do
-  #   language = Language.new(name: 'C')
-  #   label_a = Label.new(name: 'label a')
-  #   label_b = Label.new(name: 'label b')
-  #   Issue.create(
-  #     language: language,
-  #     labels: [label_a, label_b],
-  #     url: 'https://example.com',
-  #     user_avatar_url: 'https://example.com/image.png'
-  #   )
-
-  #   get issues_path
-  #   expected = ["label a", "label b"]
-  #   actual = assigns(:issues).first.labels.map { |label| label.name }
-  #   assert_equal  expected, actual
-  # end
+    get issues_path
+    expected = ["label a", "label b"]
+    actual = assigns(:issues).first.labels.map { |label| label.name }
+    assert_equal  expected, actual
+  end
 end
