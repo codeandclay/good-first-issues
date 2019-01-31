84f8351b076e658874d797c043d817234968d3ed
O Denman <oliver@codeandclay.com>
2019-01-31 17:10:57 +0000

    Test the expected issues are displayed when language and labels are provided

--- test/integration/issues_index_test.rb
+++ test/integration/issues_index_test.rb
@@ -15,4 +15,26 @@ class IssuesIndexTest < ActionDispatch::IntegrationTest
     get '/issues?labels=label+a'
     assert_select 'a', 'label b'
   end
+
+  test 'an issue will be shown that matches the provided label and language params' do
+    language_a = Language.new(name: 'C')
+    language_b = Language.new(name: 'D')
+    label_a = Label.new(name: 'label a')
+    label_b = Label.new(name: 'label b')
+    Issue.create(
+      language: language_a,
+      labels: [label_a],
+      url: 'https://example.com',
+      user_avatar_url: 'https://example.com/image.png'
+    )
+    Issue.create(
+      language: language_b,
+      labels: [label_a],
+      url: 'https://example.com',
+      user_avatar_url: 'https://example.com/image.png'
+    )
+    get '/issues?label=label+a&language=C'
+    assert_select 'a', 'C'
+    assert_select 'a', 'label a'
+  end
 end
