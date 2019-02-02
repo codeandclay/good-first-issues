94caa3742bf548eab8c4508efc1e4f1cebc904cd
O Denman <oliver@codeandclay.com>
2019-02-02 21:29:17 +0000

    Add regression tests for tage filtering

--- test/controllers/issues_controller_test.rb
+++ test/controllers/issues_controller_test.rb
@@ -1,6 +1,35 @@
 require 'test_helper'
 
 class IssuesControllerTest < ActionDispatch::IntegrationTest
+    def setup
+    set_instance_variables = Proc.new { |k,v| instance_variable_set("@#{k}", v) }
+
+    @languages = {
+      'language_a' => 'Language A',
+      'language_b' => 'Language B',
+      'language_c' => 'Language C',
+      'language_d' => 'Language D'
+    }.map do |k,v|
+      instance_variable_set("@#{k}", v)
+    end
+
+    @labels = {
+      'label_a' => 'Label A',
+      'label_b' => 'Label B',
+      'label_c' => 'Label C',
+      'label_d' => 'Label D'
+    }.map do |k,v|
+      instance_variable_set("@#{k}", v)
+    end
+
+    @issues = @languages.zip(@labels).map do |language, label|
+      Issue.create(
+        language: Language.create(name: language),
+        labels: [Label.create(name: label)]
+      )
+    end
+  end
+
   test 'The controller responds 200 when there are issues' do
     language = Language.new(name: 'C')
     label_a = Label.new(name: 'label a')
@@ -44,5 +73,19 @@ class IssuesControllerTest < ActionDispatch::IntegrationTest
     expected = ["label a", "label b"]
     actual = assigns(:issues).first.labels.map { |label| label.name }
     assert_equal  expected, actual
+  end
+
+  test 'an issue will be displayed with all its labels when only one is provided in params' do
+    @issues.first.labels << Label.create(name: 'Label X')
+    get '/issues?labels=Label+A'
+    actual = assigns(:issues).first.labels.map(&:name)
+    assert_equal ['Label A', 'Label X'], actual
+  end
+
+  test 'issues will be shown that intersecy the provide labels and languages params' do
+    @issues.first.labels << Label.create(name: 'Label X')
+    @issues.last.labels << Label.first_or_create(name: 'Label X')
+    get '/issues?languages=Language+A&labels=Label+X'
+    assert_equal 1, assigns(:issues).count
   end
 end
