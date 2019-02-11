13dd800725d600b16272ea90b9435140d64b0dc2
O Denman <oliver@codeandclay.com>
2019-02-11 15:40:16 +0000

    Remove all issues page
    Slight pivot. I'm narrowing the scope of this project to just Ruby issues due
    to database costs. Along with that I've decided to make this a single page
    app.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :issues, only: :index
+  resources :issues, only: [:index, :create]
   resources :filtered_tags, only: :create
-  resources :tags, only: :index
+  # resources :tags, only: :index
 
   root to: 'issues#index'
 end
